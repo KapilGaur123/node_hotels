b9ac1c6cd63969316106f79fb370db80a175ec13
Your Name <your.email@example.com>
2024-08-17 00:50:23 +0530

    debugging the error

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 // const mongoURL = process.env.MONGODB_URL_LOCAL; 
 
 // define the mongoDB server connection but this is use for hosting 
-const MONGODB_URL = process.env.DB_URL;
+const mongoURL = process.env.MONGODB_URL;
 
 // set up mongo connection
-mongoose.connect(MONGODB_URL);
+mongoose.connect(mongoURL);
 
 const db = mongoose.connection;
 
